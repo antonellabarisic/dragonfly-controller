65a783669db1ccc479e4a48dc40435de386d1ed2
John Ericksen <johncarl81@gmail.com>
2020-11-01 12:25:27 -0700

    Swapped out map for 3d scene

--- src/dragonfly_dashboard/dashboard/src/main/java/edu/unm/dragonfly/DashboardController.java
+++ src/dragonfly_dashboard/dashboard/src/main/java/edu/unm/dragonfly/DashboardController.java
@@ -2,11 +2,8 @@ package edu.unm.dragonfly;
 
 import com.esri.arcgisruntime.concurrent.ListenableFuture;
 import com.esri.arcgisruntime.geometry.*;
-import com.esri.arcgisruntime.internal.jni.CoreGeoprocessingLinearUnits;
-import com.esri.arcgisruntime.mapping.ArcGISScene;
-import com.esri.arcgisruntime.mapping.ArcGISTiledElevationSource;
-import com.esri.arcgisruntime.mapping.Basemap;
-import com.esri.arcgisruntime.mapping.Surface;
+import com.esri.arcgisruntime.loadable.LoadStatus;
+import com.esri.arcgisruntime.mapping.*;
 import com.esri.arcgisruntime.mapping.view.*;
 import com.esri.arcgisruntime.symbology.*;
 import io.reactivex.Observable;
@@ -24,16 +21,19 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.geometry.Point2D;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
-import javafx.scene.control.TextInputDialog;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
 import org.ros.exception.ServiceNotFoundException;
 import org.ros.node.ConnectedNode;
 import org.ros.node.topic.Subscriber;
 
 import javax.inject.Inject;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.*;
@@ -51,7 +51,7 @@ public class DashboardController {
     private static final Random RAND = new Random(System.currentTimeMillis());
 
     @FXML
-    private SceneView sceneView;
+    private StackPane mapPlaceholder;
     @FXML
     private Button add;
     @FXML
@@ -83,6 +83,7 @@ public class DashboardController {
     private final GraphicsOverlay pathOverlay = new GraphicsOverlay();
     private final List<Point> boundaryPoints = new ArrayList<Point>();
     private CoordianteSelectionMode mode = CoordianteSelectionMode.CLEAR;
+    SceneView sceneView;
 
     private enum CoordianteSelectionMode {
         SELECT("Finished"),
@@ -100,81 +101,26 @@ public class DashboardController {
     private ConnectedNode node;
 
     public void initialize() {
-        // create a scene and add a basemap to it
-        ArcGISScene scene = new ArcGISScene();
-        scene.setBasemap(Basemap.createImagery());
+        sceneView = new SceneView();
+        // load a mobile scene package
+        final String mspkPath = new File("/home/john/dev/dragonfly/map.mspk").getAbsolutePath();
+        MobileScenePackage mobileScenePackage = new MobileScenePackage(mspkPath);
 
-        sceneView.setArcGISScene(scene);
+        mobileScenePackage.loadAsync();
+        mobileScenePackage.addDoneLoadingListener(() -> {
 
-        sceneView.getGraphicsOverlays().add(droneOverlay);
-        droneOverlay.getSceneProperties().setSurfacePlacement(LayerSceneProperties.SurfacePlacement.RELATIVE);
-        sceneView.getGraphicsOverlays().add(droneShadowOverlay);
-        droneShadowOverlay.getSceneProperties().setSurfacePlacement(LayerSceneProperties.SurfacePlacement.DRAPED_FLAT);
-        sceneView.getGraphicsOverlays().add(boundaryOverlay);
-        boundaryOverlay.getSceneProperties().setSurfacePlacement(LayerSceneProperties.SurfacePlacement.DRAPED_FLAT);
-        sceneView.getGraphicsOverlays().add(pathOverlay);
-        pathOverlay.getSceneProperties().setSurfacePlacement(LayerSceneProperties.SurfacePlacement.RELATIVE);
+            if (mobileScenePackage.getLoadStatus() == LoadStatus.LOADED && mobileScenePackage.getScenes().size() > 0) {
+                // set the first scene from the package to the scene view
+                sceneView.setArcGISScene(mobileScenePackage.getScenes().get(0));
 
-        sceneView.setOnMouseMoved(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                Point2D point2D = new Point2D(event.getX(), event.getY());
-
-                // get the scene location from the screen position
-                ListenableFuture<Point> pointFuture = sceneView.screenToLocationAsync(point2D);
-                pointFuture.addDoneListener(() -> {
-                    try {
-                        Point point = pointFuture.get();
-                        coordinates.setText("Lat: " + point.getY() + " Lon: " + point.getX());
-
-                    } catch (InterruptedException | ExecutionException e) {
-                        e.printStackTrace();
-                    }
-                });
+                initalizeScene();
+            } else {
+                Alert alert = new Alert(Alert.AlertType.ERROR, "Failed to load the mobile scene package");
+                alert.show();
             }
         });
 
-        sceneView.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                if(mode == CoordianteSelectionMode.SELECT) {
-                    Point2D point2D = new Point2D(event.getX(), event.getY());
-
-                    // get the scene location from the screen position
-                    ListenableFuture<Point> pointFuture = sceneView.screenToLocationAsync(point2D);
-                    pointFuture.addDoneListener(() -> {
-                        try {
-                            Point point = pointFuture.get();
-
-                            boundaryPoints.add(point);
-
-                            boundaryOverlay.getGraphics().clear();
-
-                            Graphic boudaryGraphic;
-                            if(boundaryPoints.size() == 1) {
-                                SimpleMarkerSymbol markerSymbol = new SimpleMarkerSymbol(SimpleMarkerSymbol.Style.CIRCLE, RED, 2);
-                                boudaryGraphic = new Graphic(new Point(boundaryPoints.get(0).getX(), boundaryPoints.get(0).getY()), markerSymbol);
-                            } else if(boundaryPoints.size() == 2) {
-                                PolylineBuilder lineBuilder = new PolylineBuilder(SpatialReferences.getWgs84());
-                                lineBuilder.addPoint(boundaryPoints.get(0).getX(), boundaryPoints.get(0).getY());
-                                lineBuilder.addPoint(boundaryPoints.get(1).getX(), boundaryPoints.get(1).getY());
-                                SimpleLineSymbol lineSymbol = new SimpleLineSymbol(SimpleLineSymbol.Style.SOLID, RED, 1);
-                                boudaryGraphic = new Graphic(lineBuilder.toGeometry(), lineSymbol);
-                            } else {
-                                PointCollection polygonPoints = new PointCollection(boundaryPoints);
-                                SimpleFillSymbol polygonSymbol = new SimpleFillSymbol(SimpleFillSymbol.Style.SOLID, ALPHA_RED, new SimpleLineSymbol(SimpleLineSymbol.Style.SOLID, RED, .3f));
-                                boudaryGraphic = new Graphic(new Polygon(polygonPoints), polygonSymbol);
-                            }
-                            boundaryOverlay.getGraphics().add(boudaryGraphic);
-
-
-                        } catch (InterruptedException | ExecutionException e) {
-                            e.printStackTrace();
-                        }
-                    });
-                }
-            }
-        });
+        mapPlaceholder.getChildren().add(sceneView);
 
         select.setOnAction(new EventHandler<ActionEvent>() {
             @Override
@@ -195,9 +141,9 @@ public class DashboardController {
         });
 
         // add base surface for elevation data
-        Surface surface = new Surface();
-        surface.getElevationSources().add(new ArcGISTiledElevationSource(ELEVATION_IMAGE_SERVICE));
-        scene.setBaseSurface(surface);
+//        Surface surface = new Surface();
+//        surface.getElevationSources().add(new ArcGISTiledElevationSource(ELEVATION_IMAGE_SERVICE));
+//        scene.setBaseSurface(surface);
 
         drones.setItems(droneList);
         log.setItems(logList);
@@ -396,6 +342,78 @@ public class DashboardController {
                 .subscribe(name -> addDrone(name));
 
         log("Dashboard Startup");
+    }
+
+    private void initalizeScene() {
+        sceneView.getGraphicsOverlays().add(droneOverlay);
+        droneOverlay.getSceneProperties().setSurfacePlacement(LayerSceneProperties.SurfacePlacement.RELATIVE);
+        sceneView.getGraphicsOverlays().add(droneShadowOverlay);
+        droneShadowOverlay.getSceneProperties().setSurfacePlacement(LayerSceneProperties.SurfacePlacement.DRAPED_FLAT);
+        sceneView.getGraphicsOverlays().add(boundaryOverlay);
+        boundaryOverlay.getSceneProperties().setSurfacePlacement(LayerSceneProperties.SurfacePlacement.DRAPED_FLAT);
+        sceneView.getGraphicsOverlays().add(pathOverlay);
+        pathOverlay.getSceneProperties().setSurfacePlacement(LayerSceneProperties.SurfacePlacement.RELATIVE);
+
+        sceneView.setOnMouseMoved(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                Point2D point2D = new Point2D(event.getX(), event.getY());
+
+                // get the scene location from the screen position
+                ListenableFuture<Point> pointFuture = sceneView.screenToLocationAsync(point2D);
+                pointFuture.addDoneListener(() -> {
+                    try {
+                        Point point = pointFuture.get();
+                        coordinates.setText(String.format("Lat: %.6f Lon: %.6f Alt: %.4f", point.getY(), point.getX(), point.getZ()));
+
+                    } catch (InterruptedException | ExecutionException e) {
+                        e.printStackTrace();
+                    }
+                });
+            }
+        });
+
+        sceneView.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                if (mode == CoordianteSelectionMode.SELECT) {
+                    Point2D point2D = new Point2D(event.getX(), event.getY());
+
+                    // get the scene location from the screen position
+                    ListenableFuture<Point> pointFuture = sceneView.screenToLocationAsync(point2D);
+                    pointFuture.addDoneListener(() -> {
+                        try {
+                            Point point = pointFuture.get();
+
+                            boundaryPoints.add(point);
+
+                            boundaryOverlay.getGraphics().clear();
+
+                            Graphic boudaryGraphic;
+                            if (boundaryPoints.size() == 1) {
+                                SimpleMarkerSymbol markerSymbol = new SimpleMarkerSymbol(SimpleMarkerSymbol.Style.CIRCLE, RED, 2);
+                                boudaryGraphic = new Graphic(new Point(boundaryPoints.get(0).getX(), boundaryPoints.get(0).getY()), markerSymbol);
+                            } else if (boundaryPoints.size() == 2) {
+                                PolylineBuilder lineBuilder = new PolylineBuilder(SpatialReferences.getWgs84());
+                                lineBuilder.addPoint(boundaryPoints.get(0).getX(), boundaryPoints.get(0).getY());
+                                lineBuilder.addPoint(boundaryPoints.get(1).getX(), boundaryPoints.get(1).getY());
+                                SimpleLineSymbol lineSymbol = new SimpleLineSymbol(SimpleLineSymbol.Style.SOLID, RED, 1);
+                                boudaryGraphic = new Graphic(lineBuilder.toGeometry(), lineSymbol);
+                            } else {
+                                PointCollection polygonPoints = new PointCollection(boundaryPoints);
+                                SimpleFillSymbol polygonSymbol = new SimpleFillSymbol(SimpleFillSymbol.Style.SOLID, ALPHA_RED, new SimpleLineSymbol(SimpleLineSymbol.Style.SOLID, RED, .3f));
+                                boudaryGraphic = new Graphic(new Polygon(polygonPoints), polygonSymbol);
+                            }
+                            boundaryOverlay.getGraphics().add(boudaryGraphic);
+
+
+                        } catch (InterruptedException | ExecutionException e) {
+                            e.printStackTrace();
+                        }
+                    });
+                }
+            }
+        });
     }
 
     private boolean inside(Point randomPoint, List<Point> boundaryPoints) {
